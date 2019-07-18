2d118cf0f956148a1b876487586f233f7a041930
David Ingram <david@ingramaviation.com>
2019-07-18 19:12:28 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,10 @@
 def square_array(array)
+  squared = []
   counter = 0 
+  
   while counter < array.length do
-    puts square_array
+    squared << (array[counter] ** 2)
+    counter += 1 
+  end
+  squared
 end
